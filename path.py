6ecbd8229220d92cc5e3d5b36322cc3831fe9184
jhammelm <jhammelm@mit.edu>
2020-03-10 16:06:39 -0400

    fix dijkstra bugs

--- path.py
+++ path.py
@@ -10,20 +10,24 @@ def dijkstra(u,v,distances,indices):
     preds = np.zeros((indices.shape[0]))
     while True:
         next_i = [(j,indices[j,ii],ii) for j in prev_i for ii in range(indices[j,:].shape[0])]
+
+        min_ind = None
+        min_min = None
         for ind in next_i:
-            min_ind = None
-            min_min = None
+
             if sptset[ind[1]] == 0.0:
                 if min_ind == None:
                     min_ind = ind[1]
                     min_min = dists[ind[0]]+distances[ind[0],ind[2]]
                     preds[ind[1]] = ind[0]
-                elif min_min < (dists[ind[0]]+distances[ind[0],ind[2]]):
+                elif min_min > (dists[ind[0]]+distances[ind[0],ind[2]]):
+
                     min_ind = ind[1]
                     min_min = dists[ind[0]]+distances[ind[0],ind[2]]
                     preds[ind[1]] = ind[0]
+
         sptset[min_ind] = 1.0
-        dists[min_ind] = min_min
+        dists[min_ind] = min_min 
         prev_i = [j for j in range(distances.shape[0]) if sptset[j] == 1.0]
         if min_ind == v:
             break
@@ -54,6 +58,21 @@ def sequence_path(args,data,k):
 
     return [X[p,:].reshape(data.shape[1:]) for p in path],nsteps
     
-                                        
+
+#unit tests change numbers to test path
+# 0-1-3
+# |
+# 2
+# |
+# 4
+'''
+distances = [[1,40],[1,35],[40,3],[35,1],[4,1]]
+indices = [[1,2],[0,3],[0,4],[1,4],[2,3]]
+nddist = np.array([np.array(xi) for xi in distances])
+ndinds = np.array([np.array(xi) for xi in indices])
+sp = shortest_path(0,4,
+                   nddist,
+                   ndinds)
+print(sp)
     
-    
+'''
